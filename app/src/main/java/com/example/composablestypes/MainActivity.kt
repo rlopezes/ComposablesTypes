1b3f6767a03e38922842da4458572d1f52b6576c
rlopezes <rlopez.es@gmail.com>
2023-09-26 22:50:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composablestypes/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.composablestypes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composablestypes.ui.theme.ComposablesTypesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposablesTypesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposablesTypesScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposablesTypesScreen() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableCard(
+                "Text composable",
+                "Displays text and follows the recommended Material Design guidelines.",
+                Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableCard(
+                "Image  composable",
+                "Creates a composable that lays out and draws a given Painter class object.",
+                Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableCard(
+                "Row composable",
+                "A layout composable that places its children in a horizontal sequence.",
+                Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableCard(
+                "Column composable",
+                "A layout composable that places its children in a vertical sequence.",
+                Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableCard(title: String,
+                   description: String,
+                   backgroundColor: Color,
+                   modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposablesTypesScreenPreview() {
+    ComposablesTypesTheme {
+        ComposablesTypesScreen()
+    }
+}
